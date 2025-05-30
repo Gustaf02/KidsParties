d12ee71e5410e0118e945708831da1952945b350
Gustavo Ortiz <92409193+Gustaf02@users.noreply.github.com>
2025-05-29 21:10:59 -0300

    Correcciones en salones y en admin para optmizar el código

--- Eventos_infantiles/js/salones.js
+++ Eventos_infantiles/js/salones.js
@@ -1,18 +1,8 @@
-
-
 const LOCAL_STORAGE_KEY = 'salonesEventos';
 
 // Inicializar LocalStorage 
 if (!localStorage.getItem(LOCAL_STORAGE_KEY)) {
-    const initialSalones = [
-        { id: 1, nombre: "Princesas Encantadas", capacidad: 30, precio: 20000, fecha: "2025-12-01" },
-        { id: 2, nombre: "Game Over Party", capacidad: 25, precio: 25000, fecha: "2025-12-02" }, 
-        { id: 3, nombre: "Mi primer año", capacidad: 40, precio: 85000, fecha: "2025-12-02" },
-        { id: 4, nombre: "Caritas Creativas", capacidad: 20, precio: 90000, fecha: "2025-12-02" }
-
-
-    ];
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(initialSalones));
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify([])); 
 }
 
 // Función para obtener todos los salones
@@ -36,6 +26,7 @@ function addSalon(salon) {
         console.error("Error al agregar salón:", error);
     }
 }
+
 // Función para modificar un salón
 function updateSalon(updatedSalon) {
     try {
@@ -51,6 +42,7 @@ function updateSalon(updatedSalon) {
 }
 
 // Función para eliminar un salón
+
 function deleteSalon(id) {
     try {
         const salones = getSalones().filter(salon => salon.id !== id);
